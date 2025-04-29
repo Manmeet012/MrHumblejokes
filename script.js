4bef81709d8fd694ad62e8389783119504a377a9
Manmeet <67998120+Manmeet012@users.noreply.github.com>
2025-04-29 10:02:57 +0530

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const jokeForm = document.getElementById('jokeForm') ;
-const jokeText = document.getElementById('jokeText') ;
-const firstNameI = document.getElementById('firstName') ;
-const lastNameI = document.getElementById('lastName') ;
+const jokeForm = document.getElementById('jokeForm');
+const jokeText = document.getElementById('jokeText');
+const firstNameI = document.getElementById('firstName');
+const lastNameI = document.getElementById('lastName');
 
+jokeForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-jokeForm.addEventListener('submit', (e) =>
-{
-    e.preventDefault();
+  const firstName = firstNameI.value || 'Mr';
+  const lastName = lastNameI.value || 'Humble';
 
-const firstName = firstNameI.value || "Mr";
-const lastName = lastNameI.value || "Humble";
+  const url = `https://v2.jokeapi.dev/joke/Any?safe-mode&type=single&lang=en&firstName=${firstName}&lastName=${lastName}`;
 
-const url = `https://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`;
-fetch(url)
-    .then((res) => {
-        return res.json();
-    })
+  fetch(url)
+    .then((res) => res.json())
     .then((data) => {
-      
-       jokeText.innerHTML = data.value.joke;
+      if (data.error) {
+        jokeText.innerHTML = 'Failed to fetch a joke. Please try again later.';
+      } else {
+        jokeText.innerHTML = data.joke || 'No joke found!';
+      }
+    })
+    .catch((err) => {
+      jokeText.innerHTML = `Error: ${err.message}`;
+      console.error(err);
     });
 });
